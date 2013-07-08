cece1e08cd18e945aae8fed906ae6c56ff7ef68b
Taras <tarasdmytrus@gmail.com>
2013-07-08 19:38:44 +0300

    Update main.py
    with notification and picture from desctop

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from urllib2 import *
 from Tkinter import *
 import socket
 from ast import literal_eval
+import subprocess
 
 class Connection_timed_out():
   def __init__(self, arg):
@@ -26,7 +27,7 @@ class Main(Frame):
   def widgets(self):
     self.city_name = StringVar()
     self.city_name.set('City')
-    
+
     self.label_city = LabelFrame(self.parent, text = "Choose your city", padx=1, pady=5, bg='#DF7401', fg='#8A4B08')
     self.label_city.pack(ipadx=10, ipady=10)
 
@@ -42,6 +43,8 @@ class Main(Frame):
   def change_city(self):
     self.city_name.set("Your city is " + self.city_name_base.get())
     self.city_name_1 = self.city_name_base.get()
+    
+    self.city_name_1 = 'lviv'    # just for testing delete afterwards
 
     child = Child(self.city_name_1)
 
@@ -64,11 +67,41 @@ class Child(Main):
     if self.cities['count'] > 0:
       self.city = self.cities['list'][0]
 
-      #for line in (self.city['main']):
-      print(self.city['main'])
+      self.max_temp = (self.city['main']['temp_max'])
+      self.temp_min = (self.city['main']['temp_min'])
+      self.pressure = (self.city['main']['pressure'])
+      self.temp = (self.city['main']['temp'])
+      self.humidity = (self.city['main']['humidity'])
+ 
+      self.main = str((self.city['weather'])).split(',')
+
+      self.desc = ''
+      for line in self.main:
+        self.match = re.search(r"description.+u'(.+)'", line )
+        if self.match:
+          self.description = self.match.group(1).rstrip()
+           
+      self.desc1 = self.description.split(' ') 
+      self.desc = "_".join(self.desc1)
+    self.fetch_pictures()
+
+  def fetch_pictures(self):
+    self.proxy = ProxyHandler({'http': '172.17.35.1:8080'})    #just comment 3 lines if you don't use proxy
+    self.opener = build_opener(self.proxy)
+    install_opener(self.opener)
+
+    self.img = urlopen("http://openweathermap.org/img/w/10d.png").read()
 
 
-    
+    self.notify()
+
+
+  def notify(self):
+     temp = subprocess.Popen(["notify-send -u critical 'Temperature in "+self.city_name+"' "+str(self.temp)+" -i /afs/ericpol.int/home/x/d/xdmy/home/Desktop/10d.png"], stdout=subprocess.PIPE, shell= True).communicate()[0]      
+      
+     temp = subprocess.Popen(["notify-send 'Weather in "+self.city_name+"' "+str(self.desc)+" "], stdout=subprocess.PIPE, shell= True).communicate()[0]
+
+
 def main():
   root = Tk()
   app_gui = Main(root)
@@ -81,9 +114,24 @@ if __name__ == '__main__':
   main()
   
 '''
+[{
+u'main': u'Clouds', 
+u'id': 803, 
+u'icon': u'04d', 
+u'description': 
+u'broken clouds'
+}]
+
+
 http://proxylv.ericpol.int/epol.proxy
 
-{u'pressure': 1027, u'temp_min': 24, u'temp_max': 25.149999999999999, u'temp': 24, u'humidity': 47}
+{
+u'pressure': 1027, 
+u'temp_min': 24, 
+u'temp_max': 25.149999999999999, 
+u'temp': 24, 
+u'humidity': 47
+}
 
 {u'humidity': 81,
 u'pressure': 1012,
